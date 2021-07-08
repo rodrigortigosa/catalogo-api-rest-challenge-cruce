164a58db17138f0630642ef34306bc45f419b624
rodrigortigosa <rodrigortigosa@gmail.com>
2021-07-08 16:59:34 -0300

    Agrego codigo para permitir las politcas de CORS

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+
+//para permitir las politicas de CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 const productsJSON = require("./products.json");
 
 const PORT = 3001;
