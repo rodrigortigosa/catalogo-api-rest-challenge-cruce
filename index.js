a1bf7a44d2f907712be349ac7f2b95e98c3a3682
rodrigortigosa <rodrigortigosa@gmail.com>
2021-07-08 17:34:38 -0300

    Agrego enpoint para agregar nuevos productos

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 //para permitir las politicas de CORS
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -11,12 +13,12 @@ app.use(function (req, res, next) {
   next();
 });
 
-const productsJSON = require("./products.json");
-
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+let products = require("./products.json");
 
 const baseURL = "/api";
 
@@ -24,8 +26,23 @@ app.get(baseURL, (request, response) => {
   response.send("<h1>API Rest de catalogo para el challenge de Cruce</h1>");
 });
 
-let products = productsJSON;
-
 app.get(baseURL + "/products", (request, response) => {
   response.json(products);
+});
+
+let idCount = products.length + 1;
+
+app.post(baseURL + "/products", (request, response) => {
+  const product = request.body;
+
+  const newProduct = {
+    id: idCount,
+    name: product.name,
+    image: product.image,
+    price: product.price,
+  };
+
+  products = products.concat(newProduct);
+
+  response.json(newProduct);
 });
